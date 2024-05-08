aa54e77c52d5ad965cd410281b251c2ba856518f
waouooo <waouooo@users.noreply.github.com>
2024-05-08 16:36:57 +0000

    feat: update hugo-theme-relearn version waouooo/blog@d660c51e6fbe8655bd1a0fcdf6b53fc8bfa0dbad

---
+++ pir/index.search.js
@@ -0,0 +1,1 @@
+var relearn_search_index=[{breadcrumb:"",content:"",description:"",tags:[],title:"Cap'n Hugo Relearrrn Theme",uri:"/pir/index.html"},{breadcrumb:"",content:"",description:"",tags:[],title:"Categorrries",uri:"/pir/categories/index.html"},{breadcrumb:"",content:"",description:"",tags:[],title:"Taks",uri:"/pir/tags/index.html"}]
